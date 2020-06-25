510790b53d22068e562b76c3cd6756b7072b8855
saisitaram2000 <saisitaramvaddavalli@gmail.com>
2020-06-25 21:10:36 +0530

    created templates and added 404page

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const fs = require('fs'); //allows to work with filesystem on ur computer
 const http = require('http');
 const url = require('url');
+const { RSA_NO_PADDING } = require('constants');
 const json=fs.readFileSync(`${__dirname}/data/data.json`,'utf-8');
 console.log(__dirname);
 const laptopData=JSON.parse(json);
-console.log(JSON.parse(json));
+//console.log(JSON.parse(json));
 
 const server=http.createServer((req,res) =>{
 
@@ -12,15 +13,51 @@ const server=http.createServer((req,res) =>{
     const id=url.parse(req.url,true).query.id;
     console.log(url.parse(req.url,true));
 
+    //PRODUCTS OVERVIEW
     if(pathName==='/products' || pathName==='/'){
         res.writeHead(200,{'Content-Type':'text/html'});
-        res.end('this is PRODUCTS page');
-    }else if(pathName==='/laptop' && id<=laptopData.length){
+        fs.readFile(`${__dirname}/templates/template-overview.html`,'utf-8',(err,data) =>{
+           let overviewOutput=data;
+            fs.readFile(`${__dirname}/templates/template-card.html`,'utf-8',(err,data) =>{
+             
+                const cardsOutput = laptopData.map(el => replaceTemplate(data,el)).join('');
+                overviewOutput = overviewOutput.replace('{%CARDS%}', cardsOutput)
+                console.log(cardsOutput);
+                res.end(overviewOutput);
+            })
+
+        })
+    }
+
+    //LAPTOP DETAILS
+    else if(pathName==='/laptop' && id<=laptopData.length){
+
         res.writeHead(200,{'Content-Type':'text/html'});
-        res.end(`this is Laptop page for laptop ${id}`);
-    }else{
+        fs.readFile(`${__dirname}/templates/template-laptop.html`,'utf-8',(err,data) =>{
+          const laptop=laptopData[id];
+          const output = replaceTemplate(data,laptop);
+            res.end(output);
+        })
+       
+    }
+    
+    //IMAGES
+    else if((/\.(jpg|jpeg|png|gif)$/i).test(pathName)){
+        fs.readFile(`${__dirname}/data/img/${pathName}`,(err,data) =>{
+            res.writeHead(200,{'Content-Type':'image/jpg'});
+            res.end(data);
+        })
+    }
+
+
+    //URL NOT FOUND
+    else{
         res.writeHead(404,{'Content-Type':'text/html'});
-        res.end('URL not found on server');
+
+        fs.readFile(`${__dirname}/templates/template-404.html`,'utf-8',(err,data)=>{
+            res.end(data);
+        })
+     
     }
     
 
@@ -28,3 +65,16 @@ const server=http.createServer((req,res) =>{
 server.listen(8080,'127.0.0.1',()=>{
     console.log('listening for requests now');
 })
+
+function replaceTemplate(originalHtml,laptop){
+    let output=originalHtml.replace(/{%PRODUCTNAME%}/g, laptop.productName);
+    output = output.replace(/{%IMAGE%}/g, laptop.image);
+    output = output.replace(/{%PRICE%}/g, laptop.price);
+    output = output.replace(/{%SCREEN%}/g, laptop.screen);
+    output = output.replace(/{%CPU%}/g, laptop.cpu);
+    output = output.replace(/{%STORAGE%}/g, laptop.storage);
+    output = output.replace(/{%RAM%}/g, laptop.ram);
+    output = output.replace(/{%DESCRIPTION%}/g, laptop.description);
+    output = output.replace(/{%id%}/g, laptop.id);
+    return output;
+}
